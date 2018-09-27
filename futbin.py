af17313f4b7a4bdc30aa8743d703b1ef8bc05c35
daceyh123 <36930977+daceyh123@users.noreply.github.com>
2018-09-27 18:56:18 -0400

    WebScraper

---
+++ futbin.py
@@ -0,0 +1,45 @@
+import bs4
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen as uOpen
+from urllib.request import Request
+
+filename = "players.csv"
+f = open(filename, "w")
+
+headers = "player, rating, price\n"
+
+f.write(headers)
+
+i = 1
+while i < 3:
+
+	my_url = 'https://www.futbin.com/19/players?page=' + str(i)
+
+	pgdownload = Request(my_url, headers={'User-Agent': 'Mozilla/5.0'})
+
+	page_html = uOpen(pgdownload).read()
+	uOpen(pgdownload).close()
+
+	page_soup = soup(page_html, "html.parser")
+
+	containers = page_soup.findAll("tr", {"class": "player_tr_1"})
+	containers2 = page_soup.findAll("tr", {"class": "player_tr_2"})
+	containers.extend(containers2)
+
+	for container in containers:
+
+		player_name = container.find("a", {"class": "player_name_players_table"}).text
+
+		if container.find("span", {"class": "form rating ut19 icon gold rare"}) is None or container.find("span", {"class": "form rating ut19 icon gold rare"}) == 0:
+
+			rating = "None"
+		else:
+			rating = container.find("span", {"class": "form rating ut19 icon gold rare"}).text
+
+		price = container.find("span", {"class": "ps4_color font-weight-bold"}).text.strip()
+
+		f.write(player_name + "," + rating + "," + price + "\n")
+	i += 1
+
+f.close()
+	
---
+++ futwiz2.py
@@ -0,0 +1,41 @@
+import bs4
+from bs4 import BeautifulSoup as soup
+from urllib.request import urlopen as uOpen
+from urllib.request import Request
+
+filename = "players.csv"
+f = open(filename, "w")
+
+headers = "player, rating, price\n"
+
+f.write(headers)
+
+i = 0
+while i < 300:
+
+	my_url = 'https://www.futwiz.com/en/fifa18/players?page=' + str(i)
+
+	pgdownload = Request(my_url, headers={'User-Agent': 'Mozilla/5.0'})
+
+	page_html = uOpen(pgdownload).read()
+	uOpen(pgdownload).close()
+
+	page_soup = soup(page_html, "html.parser")
+
+	containers = page_soup.findAll("span", {"class": "ps4_color font-weight-bold"})
+
+
+	for container in containers:
+		player_name = container.p.text.strip()
+
+		rating_container = container.find("div", {"class": "otherversion18-txt"})
+		rating = rating_container.text.strip()
+
+		price_container = container.find("td", {"width": "30"})
+		price = price_container.text.strip()
+
+		f.write(player_name + "," + rating + "," + price.replace(',', '') + "\n")
+	i += 1
+
+f.close()
+
